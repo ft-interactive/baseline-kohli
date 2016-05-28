9d11f398eb290d630525082b8f09fe73537fe101
johnburnmurdoch <j.burn.murdoch@gmail.com>
2016-05-28 14:23:03 +0100

    First commit

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -1,1 +1,22 @@
-// YOUR CODE HERE
+/* eslint-disable */
+
+queue()
+  .defer(d3.csv, './data/data.csv')
+  // .defer(d3.csv, './data/runDownAll2.csv')
+  .await(drawAll);
+
+function drawAll(error,data){
+
+	runUp(d3.select('#runUp'),data);
+
+	var originalWidth = window.innerWidth;
+
+	d3.select(window).on('resize',function(){
+		if(window.innerWidth != originalWidth){
+			runUp(d3.select('#runUp'),data);
+		}
+	});
+
+}
+
+/* eslint-enable */
